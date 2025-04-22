ae4e3c0641b97e982e1a0437baca741b8c650a2b
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-22 10:16:12 +0530

    second commit

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,3 +1,4 @@
+import { resolve } from "cypress/types/bluebird";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -11,6 +12,7 @@ const UserDetails = () => {
     async function fetchUserDetails() {
       try {
         setLoading(true);
+        await new Promise((resolve) => setTimeout(resolve,100))
         const response = await fetch(
           `https://jsonplaceholder.typicode.com/users/${id}`
         );
